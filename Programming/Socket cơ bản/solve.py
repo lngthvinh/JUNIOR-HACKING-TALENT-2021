ef453110e3016e7b6d8de4eeec6331d01d0fcc64
lngthvinh <56266496+lngthvinh@users.noreply.github.com>
2021-09-28 05:37:24 +0700

    Update solve.py

--- Programming/Socket cơ bản/solve.py
+++ Programming/Socket cơ bản/solve.py
@@ -1,4 +1,10 @@
 # !/usr/bin/python
+"""
+nc 178.128.19.56 20001
+Xin chào, ở thử thách này bạn sẽ được làm quen với socket programming. Bước 1: bạn hãy thử nhận thêm 1 hàng từ phản hồi của server nhé
+Bước 2: Bạn có thể gửi cho server số 100 không? (Sau khi gửi, bạn hãy nhớ nhận thêm 1 hàng từ phản hồi của server nhé)
+Bạn đã hết giờ để trả lời
+"""
 
 import socket
 
@@ -31,3 +37,5 @@ print(data.decode())
 data = s.recv(999999)
 print(data.decode())
 
+
+# CTF{can_you_write_your_own_script?}
