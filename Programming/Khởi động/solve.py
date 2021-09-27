e004678d6a85dcbc207c84680bde91960c7798e5
lngthvinh <56266496+lngthvinh@users.noreply.github.com>
2021-09-28 05:39:29 +0700

    Update solve.py

--- Programming/Khởi động/solve.py
+++ Programming/Khởi động/solve.py
@@ -1,4 +1,9 @@
 # !/usr/bin/python
+"""
+nc 178.128.19.56 9797
+[+] Xin chào. Bạn phải trả lời đúng 100 câu hỏi liên tiếp để lấy được flag!
+[+] Hãy chuyển 677452994679 từ hệ-10 sang hệ-8
+"""
 
 def solver(d1, d2, d3):
     # xu ly d2
@@ -47,3 +52,4 @@ while True:
 
     s.sendall(f"{str}\n".encode())
 
+# CTF{socket_is_use_to_transfer_data_over_the_internet}
